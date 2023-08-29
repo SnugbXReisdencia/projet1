fbd4877f1ddebb852d77dcd590fd02090ddd8369
SnugbXResdencia <santoseverlan29@gmail.com>
2023-08-29 09:03:31 -0300

    add exercicio 1

---
+++ Resposta_Execicios.md
@@ -0,0 +1,7 @@
+#Exercicio 1: 
+(<imgs/Captura de tela de 2023-08-29 08-24-36.png>)
+
+#Exercicio 2:
+ta no github
+
+#Exercicio 3:
---
+++ meuprog1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+
+    system("clear");
+    string nome;
+
+    cout << "Informe seu nome: ";
+
+    cin >> nome;
+
+    cout << "Bom dia " << nome << endl;
+
+    return 0;
+}
